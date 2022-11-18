7189dc57490ca1c1472260da6a8c7cdd2416a8d4
Noah Soubliere <nsoubliere@lcs.on.ca>
2022-11-18 15:06:46 -0500

    Created a loop for the circles

--- ShrinkingCircles/ContentView.swift
+++ ShrinkingCircles/ContentView.swift
@@ -14,46 +14,15 @@ struct ShrinkingCircles: Shape{
         //Create a path
         var path = Path()
         
-    
-        //First circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height, y: 0),
-                                   size: CGSize(width: rect.height, height: rect.height)))
-      
-        //Secound Circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 25, y: 25),
-                                   size: CGSize(width: rect.height-50, height: rect.height-50)))
-      
-        //Thrid circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 50, y: 50),
-                                   size: CGSize(width: rect.height-100, height: rect.height-100)))
-        
-        //Fourth Circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 75, y: 75),
-                                   size: CGSize(width: rect.height-150, height: rect.height-150)))
-        //Fith circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 100, y: 100),
-                                   size: CGSize(width: rect.height-200, height: rect.height-200)))
-        //sixth circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 125, y: 125),
-                                   size: CGSize(width: rect.height-250, height: rect.height-250)))
-        
-        //seventh circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 150, y: 150),
-                                   size: CGSize(width: rect.height-300, height: rect.height-300)))
-        
-        //eith circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 175, y: 175),
-                                   size: CGSize(width: rect.height-350, height: rect.height-350)))
-        
-        //ninth circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 185, y: 185),
-                                   size: CGSize(width: rect.height-370, height: rect.height-370)))
-        //tenth circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 195, y: 195),
-                                   size: CGSize(width: rect.height-390, height: rect.height-390)))
-        //eleventh circle
-        path.addEllipse(in: CGRect(origin: CGPoint(x: rect.width - rect.height + 197.5, y: 197.5),
-                                   size: CGSize(width: rect.height-395, height: rect.height-395)))
+        //loop for smaller circles
+        for i in 0...3 {
+            
+            let j = CGFloat(i)
+            
+            path.addEllipse(in: CGRect(origin: CGPoint(x: rect.midX - rect.midY + 25 * j, y: 25 * j),
+                                       size: CGSize(width: rect.height - 50 * j, height: rect.height - 50 * j)))
+        }
+
 
         //return the path
         return path
