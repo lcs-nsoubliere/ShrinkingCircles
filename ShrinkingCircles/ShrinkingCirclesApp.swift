03a54e04c7dad06fd77d552f600d38806fd0c83a
Noah Soubliere <nsoubliere@lcs.on.ca>
2022-11-18 14:17:59 -0500

    Initial Commit

---
+++ ShrinkingCircles/ShrinkingCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShrinkingCirclesApp.swift
+//  ShrinkingCircles
+//
+//  Created by Noah Alexandre Soubliere on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct ShrinkingCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
